4c7e96d6cf627581e5283bbed875e4f6204624a1
Alexander Ivchenko <alex1986br@yandex.ru>
2023-03-21 21:35:03 +0300

    Добавили условие для перемещения элементов во второй массив из основного

--- FINALTASK/Program.cs
+++ FINALTASK/Program.cs
@@ -1,11 +1,15 @@
-﻿string [] arrayMain = new string [4] {"Hello", "2", "world", ":-)"};
-string [] secondArray = new string [arrayMain.Length];
+﻿string[] arrayMain = new string[4] { "Hello", "2", "world", ":-)" };
+string[] secondArray = new string[arrayMain.Length];
 
-void ReplaceElementsMainToSecondArray (string [] arrayMain, string  []secondArray);
+void ReplaceElementsMainToSecondArray(string[] arrayMain, string[] secondArray);
 {
   int count = 0;
   for (int i = 0; i <= arrayMain.Length; i++)
   {
-    
+    if (arrayMain[i].Length <= 3)
+    {
+      secondArray[count] = arrayMain[i];
+      count++;
+    }
   }
 }
