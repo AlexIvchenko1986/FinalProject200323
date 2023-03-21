9bdb5d1fa9555fe4af908ebb14356e75b7756a07
Alexander Ivchenko <alex1986br@yandex.ru>
2023-03-21 21:28:53 +0300

    Добавили цикл

--- FINALTASK/Program.cs
+++ FINALTASK/Program.cs
@@ -1,3 +1,11 @@
 ﻿string [] arrayMain = new string [4] {"Hello", "2", "world", ":-)"};
 string [] secondArray = new string [arrayMain.Length];
 
+void ReplaceElementsMainToSecondArray (string [] arrayMain, string  []secondArray);
+{
+  int count = 0;
+  for (int i = 0; i <= arrayMain.Length; i++)
+  {
+    
+  }
+}
